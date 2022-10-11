4f84133606d4f8d40fb0b4624f9d443954d77978
Edward <edward@synap.ac>
2022-10-11 12:15:49 +0100

    Fix test run

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,16 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
-const { compilerOptions } = require('./tsconfig')
+const { pathsToModuleNameMapper } = require("ts-jest/dist/config");
+const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: "node",
   transform: {
-    "^.+\\.ts?$": "ts-jest"
+    "^.+\\.ts?$": "ts-jest",
   },
-  moduleFileExtensions: [
-    "ts",
-    "js"
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,js}',
-    '!src/**/*.d.ts',
-  ],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleFileExtensions: ["ts", "js"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.{ts,js}", "!src/**/*.d.ts"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
 };
