060076c20e33ba3c13fdf8ab5e3fd08c38394df3
Edward <edward@synap.ac>
2025-05-12 11:40:05 +0100

    tidy

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ const action = async () => {
     const [owner, repo] = getInput("repo").split("/");
     const failOnError = getInput("fail_on_error") === "true";
 
-    // WIP
     const client = getOctokit(githubToken);
 
     const pullRequest = await client.rest.pulls.get({
